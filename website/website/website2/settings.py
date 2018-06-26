d258b86f8a0c6b60fb3b2713b7c481813ec0ee8a
mkcullen228 <maura.cullen28@gmail.com>
2018-06-26 07:58:05 -0700

    added start of user log in page app. WIP

--- website/website/website2/settings.py
+++ website/website/website2/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_page',
+    'user_example',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/main_page/' 
